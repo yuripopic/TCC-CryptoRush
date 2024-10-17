0cdcd9f798d606f17aeb3ba0024e12666c7d677f
Lucas Forge Freo <88105038+forgelucas@users.noreply.github.com>
2024-10-17 18:38:09 -0300

    Popup na compra de criptromoedas atualizado, campo quantida é preenchido de acordo com o valor aplicado e historico de compra preenche toda a tela

--- CryptoRush/js/Dashboard.js
+++ CryptoRush/js/Dashboard.js
@@ -75,6 +75,27 @@ document.addEventListener('DOMContentLoaded', async function () {
         return valorAplicado / cotacao;
     }
 
+    // Função para atualizar a quantidade ao preencher o valor aplicado
+    async function atualizarQuantidade() {
+        const valorAplicado = parseFloat(document.getElementById('valor').value);
+        const moedaSelecionada = document.getElementById('moeda').value;
+
+        if (!isNaN(valorAplicado) && valorAplicado > 0) {
+            const cotacao = await getCotacao(moedaSelecionada); // Supondo que esta função obtenha a cotação da moeda selecionada
+
+            if (cotacao) {
+                const quantidade = calcularQuantidade(valorAplicado, cotacao);
+                document.getElementById('quantidade').value = quantidade.toFixed(8); // Atualiza o campo de quantidade
+            }
+        } else {
+            document.getElementById('quantidade').value = ''; // Limpa o campo se o valor aplicado for inválido
+        }
+    }
+
+    // Adiciona o evento input ao campo de valor aplicado
+    document.getElementById('valor').addEventListener('input', atualizarQuantidade);
+    document.getElementById('moeda').addEventListener('change', atualizarQuantidade); // Atualiza a quantidade ao selecionar uma moeda
+
     // Função para validar a compra
     async function validarCompra() {
         const valorAplicado = parseFloat(document.getElementById('valor').value);
@@ -120,20 +141,23 @@ document.addEventListener('DOMContentLoaded', async function () {
             title: 'Sucesso',
             text: `Compra realizada! Você comprou ${quantidade.toFixed(8)} de ${moedaSelecionada} por R$ ${valorAplicado.toFixed(2)}`,
             icon: 'success',
+
             confirmButtonText: 'OK'
         });
         
         // Adiciona a transação ao histórico após a compra
         addTransaction('Compra', valorAplicado, quantidade, moedaSelecionada);
         
-        // Atualiza o saldo
-        const novoSaldo = saldo - valorAplicado;
-        await atualizarSaldo(novoSaldo); // Subtrai o valor aplicado e atualiza o saldo
+        // // Atualiza o saldo
+        // const novoSaldo = saldo - valorAplicado;
+        // await atualizarSaldo(novoSaldo); // Subtrai o valor aplicado e atualiza o saldo
 
         // Atualiza o campo "Caixa atual" com o novo saldo
         const caixaAtualSpan = document.querySelector('.status span:first-child');
         caixaAtualSpan.textContent = `Caixa atual: R$ ${novoSaldo.toFixed(2)}`;
     }
+
+    
 
     // Função para adicionar uma transação ao histórico
     function addTransaction(type, valor, quantidade, moeda) {
